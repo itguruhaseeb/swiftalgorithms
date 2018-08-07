74cf8577961cbabe0c607fc0f07d5d18c0df32ff
Afsar Haseeb <haseeb.afsar@us.bosch.com>
2018-08-07 17:08:01 -0500

    Palindrome example

---
+++ palindrome/Palindrome.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Cocoa
+import Foundation
+
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+//Solution 1
+func solution( str: String ) -> Bool{
+    var reversed = ""
+
+    return str == reversed
+}
+
+//Test
+print( "Palindrome Test - \( solution(str: "abba") ? "PASS" : "FAIL" )" )
